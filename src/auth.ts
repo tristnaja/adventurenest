9e32dff766b1b00a424f08308f886df53edaafb2
tristan <tristan.alhabas@gmail.com>
2026-01-05 22:02:41 +0800

    fix: Prisma bug fix #6

--- src/auth.ts
+++ src/auth.ts
@@ -3,13 +3,17 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import authConfig from "./auth.config";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.adventurenest_POSTGRES_PRISMA_URL,
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    // If you MUST pass the URL manually, cast it to 'any' 
+    // to stop the compiler from complaining while you fix the versions
+    datasources: {
+      db: {
+        url: process.env.adventurenest_POSTGRES_PRISMA_URL,
+      },
     },
-  },
-});
+  } as any)
+}
 
 export const {
   handlers: { GET, POST },
@@ -17,7 +21,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prismaClientSingleton),
   session: { strategy: "jwt" },
   ...authConfig,
 });
