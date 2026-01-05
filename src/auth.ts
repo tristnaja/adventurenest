46839c7ea2d0ca273160f310136361a7a81c9ed7
tristan <tristan.alhabas@gmail.com>
2026-01-05 21:36:50 +0800

    fix: Prisma bug fix #2

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,9 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import authConfig from "./auth.config";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+});
 
 export const {
   handlers: { GET, POST },
