871b828325477521ad1dc08abce0911b8be0e6ec
tristan <tristan.alhabas@gmail.com>
2026-01-05 21:20:02 +0800

    fix: Prisma bug fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'prisma/config'
 
-// TEMPORARY: Hardcoding URL for debugging.
-// This is NOT a good practice and should be reverted.
-const DATABASE_URL = "postgresql://user:password@localhost:5432/adventurenest?schema=public";
-
 export default defineConfig({
-  datasources: {
-    db: {
-      url: DATABASE_URL,
-    },
+  datasource: {
+      url: process.env.DATABASE_URL,
   },
 })
